9468c2ad3dfd170a0ff8cb0098913857b82bfb4e
Furkan Taşdemir <furkantsdmr054@gmail.com>
2025-12-03 14:21:10 +0000

    testler yapılmış şekilde düzenlendi

--- models/staging/gz_raw_data/stg_gz_raw_data__raw_gz_sales.sql
+++ models/staging/gz_raw_data/stg_gz_raw_data__raw_gz_sales.sql
@@ -1,25 +1,12 @@
-with 
+with
 
-source as (
+    source as (select * from {{ source("gz_raw_data", "raw_gz_sales") }}),
 
-    select * from {{ source('gz_raw_data', 'raw_gz_sales') }}
+    renamed as (
 
-),
+        select date_date, orders_id, pdt_id as product_id, revenue, quantity from source
 
-renamed as (
+    )
 
-    select
-        date_date,
-        orders_id,
-        pdt_id as product_id,
-        revenue,
-        quantity
-
-    from source
-
-)
-
-select * from renamed
-
-
-
+select *
+from renamed
